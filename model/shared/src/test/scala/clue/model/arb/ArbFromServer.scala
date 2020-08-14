841791299b80e5fc44bf18e3e5384aa5e3a08b59
Shane Walker <swalker@gemini.edu>
2020-08-14 10:14:36 -0400

    Remove headers

--- model/shared/src/test/scala/clue/model/arb/ArbFromServer.scala
+++ model/shared/src/test/scala/clue/model/arb/ArbFromServer.scala
@@ -1,6 +1,3 @@
-// Copyright (c) 2016-2020 Association of Universities for Research in Astronomy, Inc. (AURA)
-// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
-
 package clue.model.arb
 
 import clue.model.StreamingMessage.FromServer
