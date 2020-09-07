4d659bac9fe3fa53f79dd2290a9e690fb95e6700
Raúl Piaggio <rpiaggio@gemini.edu>
2020-09-07 18:07:22 -0300

    Cleanup.

--- model/shared/src/main/scala/clue/model/macros.scala
+++ model/shared/src/main/scala/clue/model/macros.scala
@@ -12,9 +12,6 @@ class QueryData extends StaticAnnotation {
 private[clue] final class QueryDataImpl(val c: blackbox.Context) {
   import c.universe._
 
-  private[this] def isCaseClassOrSealed(clsDef: ClassDef) =
-    clsDef.mods.hasFlag(Flag.CASE) || clsDef.mods.hasFlag(Flag.SEALED)
-
   @tailrec
   private[this] def documentDef(tree: List[c.Tree]): Option[String] =
     tree match {
@@ -24,26 +21,10 @@ private[clue] final class QueryDataImpl(val c: blackbox.Context) {
     }
 
   final def expand(annottees: Tree*): Tree =
-    // println(annottees)
-    // q"""case class Data(hello: String)"""
-    // val xxx =
-    // q"..$mods object $objName extends { ..$objEarlyDefs } with ..$objParents { $objSelf => ..$objDefs }"
-    // println(xxx)
     annottees match {
-      // case List(clsDef: ClassDef) =>
-      // clsDef.
-      //   q"""
-      //     case class Data(hello: String)
-      //     $clsDef
-      //     object ${clsDef.name.toTermName} {
-      //       val x =
-      //     }
-      //   """
-
       case List(
             q"..$mods object $objName extends { ..$objEarlyDefs } with ..$objParents { $objSelf => ..$objDefs }"
           ) /* if object extends GraphQLQuery */ =>
-        // val defs = TermName(objDefs.toString)
         documentDef(objDefs) match {
 
           case None           =>
@@ -77,41 +58,9 @@ private[clue] final class QueryDataImpl(val c: blackbox.Context) {
           """
         }
 
-      // case List(clsDef: ClassDef) =>
-      //   q"""
-      //  $clsDef
-      //  object ${clsDef.name.toTermName} {
-      //  }
-      //  """
-      // case List(
-      //       clsDef: ClassDef,
-      //       q"..$mods object $objName extends { ..$objEarlyDefs } with ..$objParents { $objSelf => ..$objDefs }"
-      //     ) if isCaseClassOrSealed(clsDef) =>
-      //   q"""
-      //  $clsDef
-      //  $mods object $objName extends { ..$objEarlyDefs } with ..$objParents { $objSelf =>
-      //    ..$objDefs
-      //    $clsDef
-      //  }
-      //  """
-
       case _ =>
         c.abort(c.enclosingPosition,
                 "Invalid annotation target: must be an object extending GraphQLQuery"
         )
     }
-
-  // def impl(c: whitebox.Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
-  //   import c.universe._
-  //   val inputs                = annottees.map(_.tree).toList
-  //   val (annottee, expandees) = inputs match {
-  //     case (param: ValDef) :: (rest @ (_ :: _))  => (param, rest)
-  //     case (param: TypeDef) :: (rest @ (_ :: _)) => (param, rest)
-  //     case _                                     => (EmptyTree, inputs)
-  //   }
-  //   println("IN MACRO!")
-  //   println((annottee, expandees))
-  //   val outputs               = expandees
-  //   c.Expr[Any](Block(outputs, Literal(Constant(()))))
-  // }
 }
