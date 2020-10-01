5a471158c403f890a26e5c898388fabd570ceb7d
Raúl Piaggio <rpiaggio@gemini.edu>
2020-10-01 20:11:58 -0300

    Headers.

--- macros/src/main/scala/clue/macros/GraphQLParams.scala
+++ macros/src/main/scala/clue/macros/GraphQLParams.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2016-2020 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
 package clue.macros
 
 // Parameter order and names must match exactly between this class and annotation class.
