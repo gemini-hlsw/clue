619f984d69e9ea33e9b14e9b63bb2048015bed8d
Scala Steward <me@scala-steward.org>
2020-11-26 07:48:22 +0100

    Update cats-core, cats-testkit to 2.3.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -5,8 +5,8 @@ import sbt.librarymanagement._
 object Settings {
 
   object LibraryVersions {
-    val cats            = "2.2.0"
-    val catsEffect      = "2.2.0"
+    val cats            = "2.3.0"
+    val catsEffect      = "2.3.0"
     val circe           = "0.13.0"
     val disciplineMUnit = "1.0.3"
     val fs2             = "2.4.6"
