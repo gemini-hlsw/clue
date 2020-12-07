0a4b9d333046bbb5ce231aa3bb7120a7a6a5e280
Raúl Piaggio <rpiaggio@gemini.edu>
2020-12-07 19:24:36 -0300

    Simplify ReconnectionStrategy.

--- core/shared/src/main/scala/clue/package.scala
+++ core/shared/src/main/scala/clue/package.scala
@@ -1,6 +1,20 @@
 // Copyright (c) 2016-2020 Association of Universities for Research in Astronomy, Inc. (AURA)
 // For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
 
+import cats.syntax.all._
+import scala.concurrent.duration.FiniteDuration
+
 package object clue {
-  type WebSocketReconnectionStrategy[F[_]] = ReconnectionStrategy[F, WebSocketCloseEvent]
+  type ReconnectionStrategy[CE]      = (Int, CE) => Option[FiniteDuration]
+  type WebSocketReconnectionStrategy = ReconnectionStrategy[WebSocketCloseEvent]
+}
+
+package clue {
+  object ReconnectionStrategy {
+    def never[CE]: ReconnectionStrategy[CE] = (_, _) => none
+  }
+
+  object WebSocketReconnectionStrategy {
+    def never: WebSocketReconnectionStrategy = ReconnectionStrategy.never
+  }
 }
