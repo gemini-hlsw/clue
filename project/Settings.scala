bc7d14acfbe6bc25e5b1421dc69556f47c43ebdc
Scala Steward <me@scala-steward.org>
2021-04-03 08:17:33 +0200

    Update jawn-ast to 1.1.1

--- project/Settings.scala
+++ project/Settings.scala
@@ -11,7 +11,7 @@ object Settings {
     val disciplineMUnit = "1.0.7"
     val fs2             = "2.5.4"
     val grackle         = "0.0.44"
-    val jawn            = "1.1.0"
+    val jawn            = "1.1.1"
     val log4Cats        = "2.0.1"
     val monocle         = "2.1.0"
     val scalaFix        = scalafix.sbt.BuildInfo.scalafixVersion
