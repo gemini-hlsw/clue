09c6cb3446f5c868cbe85ace3884404b7c7f077d
Raúl Piaggio <rpiaggio@gemini.edu>
2021-04-08 19:35:41 -0300

    Fix inline fragments with multiple selections.

---
+++ gen/input/src/main/scala/test/LucumaQuery3.scala
@@ -0,0 +1,36 @@
+// Copyright (c) 2016-2021 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+// format: off
+/*
+  rules = [GraphQLGen]
+  GraphQLGen.schemaDirs = ["gen/input/src/main/resources/graphql/schemas"]
+ */
+// format: on
+package test
+
+import clue.annotation.GraphQL
+import clue.GraphQLOperation
+
+@GraphQL
+trait LucumaQuery3GQL extends GraphQLOperation[LucumaODB] {
+  val document = """
+      query {
+        observations(programId: "p-2", first: 2147483647) {
+          nodes {
+            id
+            observationTarget {
+              type: __typename
+              ... on Target {
+                target_id: id
+                target_name: name
+              }
+              ... on Asterism {
+                asterism_id: id
+                asterism_name: name
+              }
+            }
+          }
+        }
+      }"""
+}
