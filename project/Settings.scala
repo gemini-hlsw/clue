29725adb64ad8a5b228303941364f874ab3536a4
Scala Steward <me@scala-steward.org>
2021-04-26 10:11:25 +0200

    Update log4cats-core to 2.1.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -12,7 +12,7 @@ object Settings {
     val fs2             = "2.5.5"
     val grackle         = "0.0.44"
     val jawn            = "1.1.1"
-    val log4Cats        = "1.3.0"
+    val log4Cats        = "2.1.0"
     val monocle         = "2.1.0"
     val scalaFix        = scalafix.sbt.BuildInfo.scalafixVersion
     val scalaJSDom      = "1.1.0"
