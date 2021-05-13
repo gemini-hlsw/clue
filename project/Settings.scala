da415641057bdd1713a28948b3c3af17ea8d685d
Scala Steward <me@scala-steward.org>
2021-05-14 01:20:07 +0200

    Update discipline-munit to 1.0.9

--- project/Settings.scala
+++ project/Settings.scala
@@ -8,7 +8,7 @@ object Settings {
     val cats            = "2.6.1"
     val catsEffect      = "3.1.0"
     val circe           = "0.13.0"
-    val disciplineMUnit = "1.0.8"
+    val disciplineMUnit = "1.0.9"
     val fs2             = "3.0.2"
     val grackle         = "0.0.44"
     val jawn            = "1.1.1"
