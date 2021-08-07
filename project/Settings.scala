8ce87e5c19a995b59c9dc66ed3a04e1fec0921d9
Scala Steward <me@scala-steward.org>
2021-08-07 20:35:56 +0200

    Update http4s-circe to 1.0.0-M24

--- project/Settings.scala
+++ project/Settings.scala
@@ -11,7 +11,7 @@ object Settings {
     val disciplineMUnit = "1.0.9"
     val fs2             = "3.0.6"
     val grackle         = "0.0.44"
-    val http4s          = "1.0.0-M23"
+    val http4s          = "1.0.0-M24"
     val http4sJDKClient = "0.6.0-M1"
     val jawn            = "1.1.1"
     val log4Cats        = "2.1.1"
