1363d0c730eebdeec56e0735e10800661a5e6f14
Scala Steward <me@scala-steward.org>
2021-10-07 08:22:25 +0200

    Update http4s-jdk-http-client to 0.6.0-M5

--- project/Settings.scala
+++ project/Settings.scala
@@ -12,7 +12,7 @@ object Settings {
     val fs2             = "3.1.4"
     val grackle         = "0.1.9"
     val http4s          = "1.0.0-M27"
-    val http4sJDKClient = "0.6.0-M4"
+    val http4sJDKClient = "0.6.0-M5"
     val jawn            = "1.1.1"
     val log4Cats        = "2.1.1"
     val monocle         = "3.1.0"
