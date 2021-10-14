4b5d080e0d99c329e31d24bd16b93701125b184a
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2021-10-14 16:53:12 -0300

    Support sending graphql requests as get
    Signed-off-by: Carlos Quiroz <carlos.quiroz@noirlab.edu>

--- scalajs/src/main/scala/clue/js/AjaxJSBackend.scala
+++ scalajs/src/main/scala/clue/js/AjaxJSBackend.scala
@@ -3,6 +3,7 @@
 
 package clue.js
 
+import cats.syntax.all._
 import cats.effect._
 import clue._
 import clue.model.GraphQLRequest
@@ -10,30 +11,54 @@ import clue.model.json._
 import io.circe.syntax._
 import org.http4s.Uri
 import org.scalajs.dom.ext.Ajax
+import scala.scalajs.js.URIUtils
 
 import scala.concurrent.ExecutionContext.Implicits._
 import scala.util.Failure
 import scala.util.Success
 
-final class AjaxJSBackend[F[_]: Async] extends TransactionalBackend[F] {
+sealed trait AjaxMethod extends Product with Serializable
+
+object AjaxMethod {
+  case object GET  extends AjaxMethod
+  case object POST extends AjaxMethod
+}
+
+final class AjaxJSBackend[F[_]: Async](method: AjaxMethod) extends TransactionalBackend[F] {
   def request(
     uri:     Uri,
     request: GraphQLRequest
   ): F[String] =
     Async[F].async_ { cb =>
-      Ajax
-        .post(
-          url = uri.toString,
-          data = request.asJson.toString,
-          headers = Map("Content-Type" -> "application/json")
-        )
-        .onComplete {
-          case Success(r) => cb(Right(r.responseText))
-          case Failure(t) => cb(Left(t))
-        }
+      method match {
+        case AjaxMethod.POST =>
+          Ajax
+            .post(
+              url = uri.toString,
+              data = request.asJson.toString,
+              headers = Map("Content-Type" -> "application/json")
+            )
+            .onComplete {
+              case Success(r) => cb(Right(r.responseText))
+              case Failure(t) => cb(Left(t))
+            }
+
+        case AjaxMethod.GET =>
+          val variables = request.variables.foldMap(v => s"&variables=${v.noSpaces}")
+          val op        = request.operationName.foldMap(o => s"&operationName=$o")
+          Ajax
+            .get(
+              url = URIUtils.encodeURI(s"$uri?query=${request.query}$variables$op")
+            )
+            .onComplete {
+              case Success(r) => cb(Right(r.responseText))
+              case Failure(t) => cb(Left(t))
+            }
+      }
     }
 }
 
 object AjaxJSBackend {
-  def apply[F[_]: Async]: AjaxJSBackend[F] = new AjaxJSBackend[F]
+  def apply[F[_]: Async](method: AjaxMethod = AjaxMethod.POST): AjaxJSBackend[F] =
+    new AjaxJSBackend[F](method)
 }
