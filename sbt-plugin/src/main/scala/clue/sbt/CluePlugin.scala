309510d3acb79a33b6a2f4c81aaaacc05f7f2207
Arman Bilge <armanbilge@gmail.com>
2023-03-03 02:22:02 +0000

    This might work

--- sbt-plugin/src/main/scala/clue/sbt/CluePlugin.scala
+++ sbt-plugin/src/main/scala/clue/sbt/CluePlugin.scala
@@ -10,46 +10,46 @@ import Keys._
 import ScalafixPlugin.autoImport._
 
 object CluePlugin extends AutoPlugin {
+
   object autoImport {
     lazy val clueSourceDirectory = settingKey[File]("Clue input sources")
   }
   import autoImport._
 
-  override def requires = ScalafixPlugin
-
   override def buildSettings: Seq[Setting[_]] = Seq(
     scalafixDependencies += BuildInfo.organization %% BuildInfo.moduleName % BuildInfo.version
-  )
-
-  override def projectSettings: Seq[Setting[_]] = Seq(
-    Compile / sourceGenerators += Def.taskDyn {
-      val root    = (LocalRootProject / baseDirectory).value.toURI.toString
-      val graphql = LocalProject(thisProject.value.id + "-clue")
-      val from    = (graphql / Compile / sourceDirectory).value
-      val to      = (Compile / sourceManaged).value
-      val outFrom = from.toURI.toString.stripSuffix("/").stripPrefix(root)
-      val outTo   = to.toURI.toString.stripSuffix("/").stripPrefix(root)
-      Def.task {
-        (graphql / Compile / scalafix)
-          .toTask(s" GraphQLGen --out-from=$outFrom --out-to=$outTo")
-          .value
-        (to ** "*.scala").get
-      }
-    }.taskValue
   )
 
   override def derivedProjects(proj: ProjectDefinition[_]): Seq[Project] = Seq(
     Project(
       proj.id + "-clue",
       new File(proj.base.getParent(), proj.base.getName() + "-clue")
-    ).settings(
-      Compile / sourceDirectories += clueSourceDirectory.value,
-      // no publish
-      publish         := {},
-      publishLocal    := {},
-      publishArtifact := false,
-      publish / skip  := true
-    ).settings(proj.settings)
-      .enablePlugins(proj.plugins)
+    )
+      .settings(proj.settings)
+      .enablePlugins(proj.plugins, ScalafixPlugin)
+      .settings(
+        Compile / sourceDirectories := Seq(clueSourceDirectory.value),
+
+        // register generator
+        LocalProject(proj.id) / Compile / sourceGenerators += Def.taskDyn {
+          val root    = (LocalRootProject / baseDirectory).value.toPath
+          val from    = (Compile / sourceDirectory).value
+          val to      = (LocalProject(proj.id) / Compile / sourceManaged).value
+          val outFrom = root.relativize(from.toPath).normalize
+          val outTo   = root.relativize(to.toPath).normalize
+          Def.task {
+            (Compile / scalafix)
+              .toTask(s" GraphQLGen --out-from=$outFrom --out-to=$outTo")
+              .value
+            (to ** "*.scala").get
+          }
+        }.taskValue,
+
+        // no publish
+        publish         := {},
+        publishLocal    := {},
+        publishArtifact := false,
+        publish / skip  := true
+      )
   )
 }
