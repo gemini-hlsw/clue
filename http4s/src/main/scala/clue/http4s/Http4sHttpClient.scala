b7700b5f97570974d612b7d6604bfe24db45bcc0
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2026-04-15 19:22:40 -0400

    Scalafmt

--- http4s/src/main/scala/clue/http4s/Http4sHttpClient.scala
+++ http4s/src/main/scala/clue/http4s/Http4sHttpClient.scala
@@ -4,8 +4,8 @@
 package clue.http4s
 
 import cats.MonadThrow
-import cats.syntax.applicative.*
 import cats.effect.Concurrent
+import cats.syntax.applicative.*
 import clue.FetchClientImpl
 import org.http4s.Headers
 import org.http4s.Method.*
@@ -15,7 +15,11 @@ import org.http4s.client.Client
 import org.typelevel.log4cats.Logger
 
 object Http4sHttpClient {
-  def of[F[_]: {MonadThrow as F, Http4sHttpBackend as B, Logger as L}, S](uri: Uri, name: String = "", headers: Headers = Headers.empty): F[Http4sHttpClient[F, S]] = {
+  def of[F[_]: {MonadThrow as F, Http4sHttpBackend as B, Logger as L}, S](
+    uri:     Uri,
+    name:    String = "",
+    headers: Headers = Headers.empty
+  ): F[Http4sHttpClient[F, S]] = {
     val logPrefix = s"clue.FetchClientWithPars[${if (name.isEmpty) uri else name}]"
 
     val internalLogger = L.withModifiedString(s => s"$logPrefix $s")
@@ -26,9 +30,9 @@ object Http4sHttpClient {
   }
 
   def of[F[_]: {Concurrent as F, Logger as L}, S](
-    uri: Uri,
-    client: Client[F],
-    name: String,
+    uri:     Uri,
+    client:  Client[F],
+    name:    String,
     headers: Headers
   ): F[Http4sHttpClient[F, S]] = {
     val logPrefix = s"clue.FetchClientWithPars[${if (name.isEmpty) uri else name}]"
@@ -45,7 +49,11 @@ object Http4sHttpClient {
   def of[F[_]: Concurrent: Logger, S](uri: Uri, client: Client[F]): F[Http4sHttpClient[F, S]] =
     of[F, S](uri, client, "", Headers.empty)
 
-  def of[F[_]: Concurrent: Logger, S](uri: Uri, client: Client[F], name: String): F[Http4sHttpClient[F, S]] =
+  def of[F[_]: Concurrent: Logger, S](
+    uri:    Uri,
+    client: Client[F],
+    name:   String
+  ): F[Http4sHttpClient[F, S]] =
     of[F, S](uri, client, name, Headers.empty)
 
 }
