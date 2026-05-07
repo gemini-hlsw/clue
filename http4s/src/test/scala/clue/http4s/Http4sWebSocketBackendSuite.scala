73c12f1f9e526420311187f78be29b5100183575
Hugo van Rijswijk <git@hugovr.nl>
2026-05-07 22:31:26 +0200

    Formatting

--- http4s/src/test/scala/clue/http4s/Http4sWebSocketBackendSuite.scala
+++ http4s/src/test/scala/clue/http4s/Http4sWebSocketBackendSuite.scala
@@ -18,27 +18,28 @@ class Http4sWebSocketBackendSuite extends CatsEffectSuite:
 
   test("close releases high-level connection and only notifies onClose once") {
     for {
-      releases <- Ref.of[IO, Int](0)
-      closes   <- Ref.of[IO, Int](0)
-      deferred <- Deferred[IO, WSFrame.Close]
-      wsConn    = new WSConnectionHighLevel[IO]:
-                    override def send(wsf: WSDataFrame): IO[Unit] = IO.unit
-                    override def sendMany[G[_]: Foldable, A <: WSDataFrame](wsfs: G[A]): IO[Unit] =
-                      IO.unit
-                    override def receive: IO[Option[WSDataFrame]] = IO.never
-                    override def subprotocol: Option[String]       = none
-                    override def closeFrame: Deferred[IO, WSFrame.Close] = deferred
-      wsClient  = new WSClient[IO]:
-                    override def connect(request: WSRequest): Resource[IO, WSConnection[IO]] =
-                      Resource.eval(IO.raiseError(new RuntimeException("unused")))
-                    override def connectHighLevel(
-                      request: WSRequest
-                    ): Resource[IO, WSConnectionHighLevel[IO]] =
-                      Resource.make(IO.pure(wsConn))(_ => releases.update(_ + 1))
-      handler   = new WebSocketHandler[IO]:
-                    override def onMessage(connectionId: ConnectionId, msg: String): IO[Unit] = IO.unit
-                    override def onClose(connectionId: ConnectionId, event: CloseEvent): IO[Unit] =
-                      closes.update(_ + 1)
+      releases   <- Ref.of[IO, Int](0)
+      closes     <- Ref.of[IO, Int](0)
+      deferred   <- Deferred[IO, WSFrame.Close]
+      wsConn      = new WSConnectionHighLevel[IO]:
+                      override def send(wsf: WSDataFrame): IO[Unit]                                 = IO.unit
+                      override def sendMany[G[_]: Foldable, A <: WSDataFrame](wsfs: G[A]): IO[Unit] =
+                        IO.unit
+                      override def receive: IO[Option[WSDataFrame]]                                 = IO.never
+                      override def subprotocol: Option[String]                                      = none
+                      override def closeFrame: Deferred[IO, WSFrame.Close]                          = deferred
+      wsClient    = new WSClient[IO]:
+                      override def connect(request: WSRequest): Resource[IO, WSConnection[IO]] =
+                        Resource.eval(IO.raiseError(new RuntimeException("unused")))
+                      override def connectHighLevel(
+                        request: WSRequest
+                      ): Resource[IO, WSConnectionHighLevel[IO]] =
+                        Resource.make(IO.pure(wsConn))(_ => releases.update(_ + 1))
+      handler     = new WebSocketHandler[IO]:
+                      override def onMessage(connectionId: ConnectionId, msg: String): IO[Unit]     =
+                        IO.unit
+                      override def onClose(connectionId: ConnectionId, event: CloseEvent): IO[Unit] =
+                        closes.update(_ + 1)
       connection <- Http4sWebSocketBackend[IO](wsClient).connect(
                       Uri.unsafeFromString("ws://example.test/ws"),
                       handler,
