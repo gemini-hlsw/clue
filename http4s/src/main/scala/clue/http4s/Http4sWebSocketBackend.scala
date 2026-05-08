c359950c3b20a5dbb0fbef6ef016214a86d79477
Hugo van Rijswijk <git@hugovr.nl>
2026-05-08 12:01:59 +0200

    Add guaranteeCase and cleanup refactor close functions creation

--- http4s/src/main/scala/clue/http4s/Http4sWebSocketBackend.scala
+++ http4s/src/main/scala/clue/http4s/Http4sWebSocketBackend.scala
@@ -27,47 +27,52 @@ final class Http4sWebSocketBackend[F[_]: Concurrent](client: WSClient[F])
     handler:      WebSocketHandler[F],
     connectionId: ConnectionId
   ): F[WebSocketConnection[F]] =
-    for
-      (connection, release) <-
-        client
-          .connectHighLevel:
-            WSRequest(uri).withHeaders(Headers("Sec-WebSocket-Protocol" -> "graphql-transport-ws"))
-          .allocated
-      // Refs to ensure release/close are only executed once
-      releaseState          <- Ref.of[F, Boolean](false)
-      closeState            <- Ref.of[F, Boolean](false)
-      releaseNow             =
-        releaseState
-          .modify(released => true -> !released)
-          .flatMap(isNotReleased => release.whenA(isNotReleased))
-      notifyClose            =
-        (event: CloseEvent) =>
-          closeState
-            .modify(closed => true -> !closed)
-            .flatMap(isNotClosed => handler.onClose(connectionId, event).whenA(isNotClosed))
-      canceledClose          =
-        notifyClose(new GraphQLException(s"WS listener canceled. URI: [$uri]").asLeft)
-      listener              <-
-        connection.receiveStream
-          .evalTap:
-            case WSFrame.Text(data, _) => handler.onMessage(connectionId, data)
-            case WSFrame.Binary(_, _)  => Concurrent[F].unit
-          .onFinalizeCase:
-            case ExitCase.Succeeded  =>
-              connection.closeFrame.tryGet.flatMap: closeFrame =>
-                val event = closeFrame
-                  .map(close => CloseParams(close.statusCode, close.reason))
-                  .toRight:
-                    new GraphQLException(
-                      s"Unexpected clean close for WS without close frame. URI: [$uri]"
-                    )
-                notifyClose(event) >> releaseNow
-            case ExitCase.Errored(t) => notifyClose(t.asLeft) >> releaseNow
-            case ExitCase.Canceled   => canceledClose >> releaseNow
-          .compile
-          .drain
-          .start
-    yield new Http4sWSConnection(connection, listener, canceledClose, releaseNow)
+    client
+      .connectHighLevel:
+        WSRequest(uri).withHeaders(Headers("Sec-WebSocket-Protocol" -> "graphql-transport-ws"))
+      .allocated
+      .flatMap: (connection, release) =>
+        // Refs to ensure release/close are only executed once
+        val closeFunctionsF = for
+          releaseState <- Ref[F].of(false)
+          closeState   <- Ref[F].of(false)
+          releaseNow    = releaseState
+                            .modify(released => true -> !released)
+                            .flatMap(isNotReleased => release.whenA(isNotReleased))
+          notifyClose   =
+            (event: CloseEvent) =>
+              closeState
+                .modify(closed => true -> !closed)
+                .flatMap(isNotClosed => handler.onClose(connectionId, event).whenA(isNotClosed))
+          canceledClose =
+            notifyClose(new GraphQLException(s"WS listener canceled. URI: [$uri]").asLeft)
+        yield (releaseNow, notifyClose, canceledClose)
+
+        closeFunctionsF.flatMap: (releaseNow, notifyClose, canceledClose) =>
+          connection.receiveStream
+            .evalTap:
+              case WSFrame.Text(data, _) => handler.onMessage(connectionId, data)
+              case WSFrame.Binary(_, _)  => Concurrent[F].unit
+            .onFinalizeCase:
+              case ExitCase.Succeeded  =>
+                connection.closeFrame.tryGet.flatMap: closeFrame =>
+                  val event = closeFrame
+                    .map(close => CloseParams(close.statusCode, close.reason))
+                    .toRight:
+                      new GraphQLException(
+                        s"Unexpected clean close for WS without close frame. URI: [$uri]"
+                      )
+                  notifyClose(event) >> releaseNow
+              case ExitCase.Errored(t) => notifyClose(t.asLeft) >> releaseNow
+              case ExitCase.Canceled   => canceledClose >> releaseNow
+            .compile
+            .drain
+            .start
+            .map(new Http4sWSConnection(connection, _, canceledClose, releaseNow))
+            .guaranteeCase:
+              case Outcome.Succeeded(_) => Concurrent[F].unit
+              case Outcome.Errored(t)   => notifyClose(t.asLeft) >> releaseNow
+              case Outcome.Canceled()   => canceledClose >> releaseNow
 
 object Http4sWebSocketBackend:
   def apply[F[_]: Concurrent](client: WSClient[F]): Http4sWebSocketBackend[F] =
